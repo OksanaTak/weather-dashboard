01bdebd86d5f49f22df59291c2648b623bdf40c0
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-10-15 10:32:16 -0700

    Added 5 day forecast API and weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,50 @@ var APIkey="8cc5aa0691fc507164238dafdbbf0ac8"
 
 var searchButton=document.getElementById("City")
 
-
+console.log(searchButton)
 searchButton.addEventListener("click", function(event){
-    event.preventDefault ()
+    event.preventDefault()
 
     var city = document.getElementById("searchCity").value
     console.log (city)
     getWeatherData (city)
+    getFiveDaysData (city)
 })
 function getWeatherData(city) {
-    var currentWeatherAPI =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+    var currentWeatherAPI =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`;
     fetch(currentWeatherAPI)
     .then (response => {
         return response.json()
     }).then(data => {
-        console.log(data)
+        console.log(data) 
+        document.getElementById('city-name').innerText="city"+city
+        document.getElementById('weatherIcon').setAttribute("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+        document.getElementById('temperature').innerText="Temperature "+data.main.temp+"F"
+        document.getElementById('humidity').innerText="Humidity "+data.main.humidity+"%"
+        document.getElementById('windSpeed').innerText="Wind speed "+data.wind.speed+"mph"
     })
+}
+    function getFiveDaysData(city) {
+        //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+        var fiveDayWeatherAPI =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=imperial`;
+        fetch(fiveDayWeatherAPI)
 
+                .then (response => {
+            return response.json()
+        }).then(data => {
+            console.log(data) 
+            var j = 1
+            for(let i=0; i < data.list.length; i=i+8){
+            document.getElementById('date-'+(j)).innerText = data.list[i].dt_txt
+            document.getElementById('weatherIcon-'+j).setAttribute("src",`https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`)
+            document.getElementById('temperature-'+(j)).innerText="Temperature "+data.list[i].main.temp+"F"
+            document.getElementById('humidity-'+(j)).innerText="Humidity "+data.list[i].main.humidity+"%"
+            document.getElementById('windSpeed-'+(j)).innerText="Wind speed "+data.list[i].wind.speed+"mph"
+            j++
+            }
+        })
 
 
 }
+
+console.log("Js response")
